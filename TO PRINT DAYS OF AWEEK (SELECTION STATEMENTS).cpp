66cb7f6d7445d0ddd6a066c46204beb9f97ddc75
Battula Pancha Mukhesh <95120507+MukheshBattula@users.noreply.github.com>
2021-12-08 00:18:04 +0530

    Add files via upload

---
+++ TO PRINT DAYS OF AWEEK (SELECTION STATEMENTS).cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+ main()
+{
+	int days;
+	scanf("%d",&days);
+	switch(days)
+	{
+	  case 1: printf("sun");
+	           break;
+	  case 2: printf("mon");
+	          break;
+	  case 3: printf("tue");
+	          break; 
+	  case 4: printf("wed");
+	           break;
+	  case 5: printf("thurs");
+	          break;
+	  case 6: printf("fri");
+	           break;
+	  case 7: printf("sat");
+	           break;		   		         
+				
+		     
+	}
+		  
+} 
