ccf393797bf28c08276790e1646df4f1249643de
Battula Pancha Mukhesh <95120507+MukheshBattula@users.noreply.github.com>
2021-12-20 14:37:59 +0530

    Add files via upload

---
+++ TO DISPLAY 50 STARS(WHILE).cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	char ch='*';
+	int i=1,n;
+	printf("enter n value");
+	scanf("%d",&n);
+	while(i<=n)
+	{
+		printf("%c\n",ch);
+		i++
+	}
+}
---
+++ TO DISPLAY N TO 1(WHILE).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter n value");
+	scanf("%d",&n);
+	while(n>=1)
+	{
+		printf("%d\n",n);
+		n--;
+	}
+}
